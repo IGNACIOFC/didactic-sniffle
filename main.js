93ee970f988a3f026cdaa1dced100b0f62a6ed48
IGNACIOFC <45830117+IGNACIOFC@users.noreply.github.com>
2020-12-17 21:21:15 +0100

    index page finished

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+const navSlide = () => {
+    const burguer = document.querySelector(".burguer");
+    const nav = document.querySelector("nav ul");
+    const navLinks = document.querySelectorAll(".li-nav");
+    
+    burguer.addEventListener("click", ()=>{
+
+        // Toggle Nav
+        nav.classList.toggle("nav-active");
+
+        //animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
+            }
+        });
+
+        //Burguer Animation
+        burguer.classList.toggle("toggle");
+    });
+    
+    
+    
+}
+
+navSlide();
+
+
+
+
+
+
 
 /* Start animate the text rotator. */
 
